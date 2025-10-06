acbc8a35bd9bc24769ebe2092f97b55806fdb881
Wilson Gramer <12683684+WilsonGramer@users.noreply.github.com>
2025-10-06 15:43:14 -0400

    Add sequence tests

---
+++ tests/sequence.test.ts
@@ -0,0 +1,11 @@
+import test from "node:test";
+import { compileTest } from ".";
+import assert from "node:assert";
+import { HasType } from "../src/compile";
+
+test("sequence", () => {
+    const { db, placeholders, feedback } = compileTest("sequence.wipple");
+
+    assert.deepStrictEqual(db.display(placeholders[2], HasType), ["Number", "String"]);
+    assert.partialDeepStrictEqual(feedback.get(placeholders[2]), ["conflicting-types"]);
+});
