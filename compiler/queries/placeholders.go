115d02b3dff6e3f306d440fef45241be38c1d4be
Wilson Gramer <12683684+WilsonGramer@users.noreply.github.com>
2025-12-16 14:13:07 -0500

    Add nil check in placeholder feedback

--- compiler/queries/placeholders.go
+++ compiler/queries/placeholders.go
@@ -8,7 +8,7 @@ import (
 
 func Placeholder(db *database.Db, node database.Node, filter func(node database.Node) bool, f func(typecheck.Type)) {
 	if _, ok := database.GetFact[expressions.IsPlaceholder](node); ok {
-		if fact, ok := database.GetFact[typecheck.TypedFact](node); ok && len(fact.Group.Types) > 0 {
+		if fact, ok := database.GetFact[typecheck.TypedFact](node); ok && fact.Group != nil && len(fact.Group.Types) > 0 {
 			f(fact.Group.Types[0])
 		} else {
 			f(nil)
